b89cb18d9a3589f116e2988cf98761df1b83ab10
Bbrnn <113863725+Bbrnn@users.noreply.github.com>
2023-05-25 13:34:19 +0300

    Add files via upload

---
+++ Area_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define pi 3.142
+void main()
+{
+float area,r;
+printf("Enter the radius of the circle\n");
+scanf("%f",&r);
+area=pi*r*r;
+printf("Area is %.2f",area);
+}
---
+++ Division.c
@@ -0,0 +1,30 @@
+//this program performs division
+#include<stdio.h>
+void main()
+{
+    int x,y;
+    float z;
+    {
+        printf("Enter x \n");
+        scanf("%d",&x);
+        printf("Enter y \n");
+        scanf("%d",&y);
+        z=x/y;
+        if (y==0)
+        {
+            printf("Division not possible \n");
+        }
+        else
+        {
+            //th value of z will be printed with only 1 decimal
+            printf("The value of z is %.1f\n",z);
+        }
+
+    }
+}
+
+
+
+
+
+
